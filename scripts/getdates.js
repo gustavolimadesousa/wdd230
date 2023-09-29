71042d52a0fccf7bc0743ef60912e1903906e21c
gustavolimadesousa <gustavolimadesousa@hotmail.com>
2023-09-29 01:02:51 +0100

    Hope Page Enhancement

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,11 @@ document.querySelector(".currentYear").innerHTML = date;
 
 let lastModifiedDate = new Date(document.lastModified);
 document.querySelector(".lastModified").innerHTML = lastModifiedDate;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+  mainnav.classList.toggle('show');
+  hambutton.classList.toggle('show');
+})
