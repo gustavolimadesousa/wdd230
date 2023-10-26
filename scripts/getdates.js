42bfd27518533df96eaed5180ab941b3b87117a9
gustavolimadesousa <gustavolimadesousa@hotmail.com>
2023-10-26 23:08:35 +0100

    Homepage update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,10 +4,3 @@ document.querySelector(".currentYear").innerHTML = date;
 let lastModifiedDate = new Date(document.lastModified);
 document.querySelector(".lastModified").innerHTML = lastModifiedDate;
 
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
-
-hambutton.addEventListener('click', () => {
-  mainnav.classList.toggle('show');
-  hambutton.classList.toggle('show');
-})
