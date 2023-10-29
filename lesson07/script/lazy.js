ca099f2b4c41cbfb911679b468d1677b0dfe5c4d
gustavolimadesousa <gustavolimadesousa@hotmail.com>
2023-10-29 20:23:11 +0000

    Lazy Loading update

---
+++ lesson07/script/lazy.js
@@ -0,0 +1,2 @@
+let lastModifiedDate = new Date(document.lastModified);
+document.querySelector(".lastModified").innerHTML = "&copy; 2023 | Gustavo Lima de Sousa | Last updated: " + lastModifiedDate;
