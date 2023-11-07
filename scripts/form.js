2e663ea331f231bd1136a678396243f764a30dcd
gustavolimadesousa <gustavolimadesousa@hotmail.com>
2023-11-07 00:47:16 +0000

    update

--- scripts/form.js
+++ scripts/form.js
@@ -1,17 +1,18 @@
-function checkPasswordMatch(confirmPasswordInput) {
-  var passwordInput = document.getElementById("password");
-  var confirmPassword = confirmPasswordInput.value;
-  var password = passwordInput.value;
+let pass1 = document.querySelector('#pass1');
+let pass2 = document.querySelector('#pass2');
+let result = document.querySelector('#error');
 
-  if (password === confirmPassword) {
-      confirmPasswordInput.setCustomValidity("");
-  } else {
-      confirmPasswordInput.setCustomValidity("Passwords do not match");
-      passwordInput.value = "";
-      confirmPasswordInput.value = "";
-      passwordInput.focus();
-  }
+
+function checkPassword () {
+    result.innerText = pass1.value == pass2.value ? 'Matching' : 'Passwords must match';
 }
+
+pass1.addEventListener('keyup', () => {
+    if (pass2.value.length != 0) checkPassword();
+})
+
+pass2.addEventListener('keyup', checkPassword);
+
 
 function updateValue(val) {
   document.getElementById("ratingOutput").textContent = val;
