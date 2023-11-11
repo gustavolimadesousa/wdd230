a5e6003ad771eacdf29511213321e2d2b34a341c
gustavolimadesousa <gustavolimadesousa@hotmail.com>
2023-11-11 17:10:19 +0000

    update directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,60 +1,32 @@
-const linksURL = "https://gustavolimadesousa.github.io/wdd230/chamber/data/members.json";
-const gridBtn = document.querySelector("#grid");
-const listBtn = document.querySelector("#list");
-const display = document.querySelector("member-list");
+    // Fetch data from members.json and display it
+    fetch('data/members.json')
+      .then(response => response.json())
+      .then(data => displayMembers(data.companies));
 
-async function getMembers() {
-  try {
-    const response = await fetch(linksURL);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      displayMembers(data);
-    } else {
-      throw Error(await response.statusText);
+    function displayMembers(members) {
+      const container = document.getElementById('members-container');
+      container.innerHTML = ''; // Clear previous content
+
+      members.forEach(member => {
+        const memberCard = document.createElement('div');
+        memberCard.classList.add('member-card');
+
+        // Customize the card structure based on your requirements
+        memberCard.innerHTML = `
+          <img src="https://gustavolimadesousa.github.io/wdd230/chamber/images/${member.image}" alt="${member.name}">
+          <h2>${member.name}</h2>
+          <p>${member.address}</p>
+          <p>Phone: ${member.phone}</p>
+          <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
+          <p>Membership Level: ${member.membership_level}</p>
+          <p>${member.other_info}</p>
+        `;
+
+        container.appendChild(memberCard);
+      });
     }
-  } catch (error) {
-    console.log(error);
-  }
-}
 
-function displayMembers(members) {
-  members.members.forEach((member) => {
-    let section = document.createElement("section");
-    let companyName = document.createElement("h2");
-    let companyAddress = document.createElement("p");
-    let companyPhone = document.createElement("p");
-    let companyWebsite = document.createElement("a");
-    let logo = document.createElement("img");
-    section.setAttribute("class", "card");
-
-    companyName.textContent = member.companyName;
-    companyAddress.textContent = member.companyAddress;
-    companyPhone.textContent = member.companyPhone;
-    companyWebsite.textContent = member.website;
-    companyWebsite.setAttribute("href", member.website);
-    companyWebsite.setAttribute("target", "_blank");
-    logo.setAttribute("src", member.logo.imageName);
-    logo.setAttribute("alt", member.logo.imageAlt);
-
-    section.appendChild(companyName);
-    section.appendChild(companyAddress);
-    section.appendChild(companyPhone);
-    section.appendChild(companyWebsite);
-    section.appendChild(logo);
-
-    document.querySelector("#member-list").appendChild(section);
-  });
-}
-
-getMembers();
-
-gridBtn.addEventListener("click", () => {
-  document.querySelector("#member-list").classList.remove("list");
-  document.querySelector("#member-list").classList.add("grid");
-});
-
-listBtn.addEventListener("click", () => {
-  document.querySelector("#member-list").classList.remove("grid");
-  document.querySelector("#member-list").classList.add("list");
-});
+    function toggleView(view) {
+      const container = document.getElementById('members-container');
+      container.className = view === 'grid' ? 'grid-view' : 'list-view';
+    }
